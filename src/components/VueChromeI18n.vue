2307692a75b6209fc7f4ef879e871a5bca1f9c12
Xvezda <xvezda@naver.com>
2019-11-04 03:53:05 +0900

    Initial commit

---
+++ src/components/VueChromeI18n.vue
@@ -0,0 +1,45 @@
+<!-- Copyright (C) 2019 Xvezda <https://xvezda.com/> -->
+<template>
+  <div v-if="show">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { replacer, pattern } from '../utils';
+
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: true
+    },
+    prehook: {
+      type: Function,
+      default: context => { context }
+    },
+    posthook: {
+      type: Function,
+      default: context => { context }
+    }
+  },
+  methods: {
+    replaceMessage: function(elements) {
+      for (let element of elements) {
+        if (element.children) this.replaceMessage(element.children);
+        if (element.text && element.text.match(pattern)) {
+          element.text = element.text.replace(pattern, replacer);
+        }
+      }
+    }
+  },
+  beforeMount() {
+    this.prehook(this);
+    if (this.$slots.default) {
+      this.replaceMessage(this.$slots.default);
+    }
+    this.posthook(this);
+    this.$emit('replaced', this);
+  },
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/* Copyright (C) 2019 Xvezda <https://xvezda.com/> */
+var path = require('path');
+var webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'vue-chrome-i18n.js',
+    library: 'VueChromeI18n',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: ['vue-loader']
+      },
+      {
+        test: /\.js$/,
+        loader: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
